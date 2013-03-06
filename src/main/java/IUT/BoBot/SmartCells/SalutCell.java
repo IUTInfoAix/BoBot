c9bebd8788174cc74ada60df3e3ac45fcfd9d9b8
Raph <raphderivas@gmail.com>
2013-03-06 14:53:00 +0100

    Salut avec test

---
+++ src/main/java/IUT/BoBot/SmartCells/SalutCell.java
@@ -0,0 +1,16 @@
+package IUT.BoBot.SmartCells;
+
+import IUT.BoBot.SmartCell;
+
+public class SalutCell implements SmartCell{
+
+	@Override
+	public String ask(String question) {
+		if(question.contains("Salut"))
+			  return "Salut!";
+			else
+			  return null;
+		
+	}
+
+}

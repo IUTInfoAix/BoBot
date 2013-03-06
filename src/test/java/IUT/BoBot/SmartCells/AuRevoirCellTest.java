fd047675fb8e7a9809740fd965ae24a875afb585
Robin HERBAUT <herbaut.robin@gmail.com>
2013-03-06 10:14:43 +0100

    Bobot peut vraiment dire au revoire

---
+++ src/test/java/IUT/BoBot/SmartCells/AuRevoirCellTest.java
@@ -0,0 +1,20 @@
+package IUT.BoBot.SmartCells;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuRevoirCellTest {
+
+	AuRevoirCell cell = new AuRevoirCell();
+
+	@Test
+	public void testAuRevoir() {
+		assertEquals("Au revoir", cell.ask("Au revoir BoBot"));
+	}
+	
+	@Test
+	public void testNotAuRevoir() {
+		assertEquals(null, cell.ask("nbbj"));
+	}
+}

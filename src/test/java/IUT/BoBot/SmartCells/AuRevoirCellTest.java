7420c80bb940d9a4287a6e3379ff3084ee681531
Pomoa <tristan.ughetti@pomoa.fr>
2014-02-26 09:23:19 +0100

    AurevoirEtSalut

---
+++ src/test/java/IUT/BoBot/SmartCells/AuRevoirCellTest.java
@@ -0,0 +1,21 @@
+package IUT.BoBot.SmartCells;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuRevoirCellTest {
+	
+	AuRevoirCell cell = new AuRevoirCell();
+
+	@Test
+	public void testAuRevoir() {
+		assertEquals("Au revoir!", cell.ask("Au revoir!"));
+	}
+	
+	@Test
+	public void testNotAuRevoir() {
+		assertEquals(null, cell.ask("Au revoir!"));
+	}
+
+}

05fb42e10ddae617cc7734ddfd16ec529de37fef
Jerome Padilla <p12023945@gmail.com>
2014-02-26 10:08:08 +0100

    Fonction Aurevoir

---
+++ src/main/java/IUT/BoBot/SmartCells/AuRevoir.java
@@ -0,0 +1,12 @@
+package IUT.BoBot.SmartCells;
+
+import IUT.BoBot.SmartCell;
+
+public class AuRevoir implements SmartCell{
+	public String ask(String question) {
+		if(question.contains("Au revoir!"))
+		  return "Au revoir!";
+		else
+		  return null;
+	}
+}
